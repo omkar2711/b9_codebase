c663c68d917a0a5206adcef6fcdd328cf656eb82
omkar2711 <omkarbharitkar20@ece.iiitp.ac.in>
2025-09-22 13:56:43 +0530

    variables and data types

---
+++ VariablesAndDataTypes/BasicJava.java
@@ -0,0 +1,46 @@
+package VariablesAndDataTypes;
+
+public class BasicJava {
+    public static void main(String[] args) {
+        //Data Types
+        //Primitive
+        //int
+        int a = 10;
+
+        //double
+        double price = 30.40;
+
+        //Char
+        char ch = 'A';
+
+        //boolean
+        boolean b = true;
+
+        //Reference Data Type
+        //String
+
+        String str = "Hello";
+
+        System.out.println( "a = "  +  a );
+        System.out.println("price = " + price );
+        System.out.println("ch = " + ch );
+        System.out.println( "b = " + b);
+        System.out.println( "str = " + str );
+
+
+        //Problem
+
+        String name = "Rushikesh";
+        int age = 21;
+        char grade = 'A';
+
+        System.out.println( "name = " + name );
+        System.out.println( "age = " + age );
+        System.out.println( "grade = " + grade );
+
+
+
+
+
+    }
+}
