0df02a7eb2a06168c95f6173689fad5cebbc7f7f
rushikesh bhagat <rushikesh@gmail.com>
2025-09-22 13:58:11 +0530

    first Problem

---
+++ Basic_java/variable.java
@@ -0,0 +1,13 @@
+package Basic_java;
+
+public class variable {
+    public static void main(String[] args) {
+       String name = "Rushikesh";
+       int age = 21;
+       char grade = 'A';
+       
+       System.out.println("name:" + name + "\n age: " + age + "\n grade: " +grade );
+
+
+    }
+}
