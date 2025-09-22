8f16e756e97f794a6131ad7f9034df40f0be11e7
parshwa15 <parshwa1508@gmail.com>
2025-09-22 13:58:23 +0530

    pr1 created

---
+++ parshwa/variables&datatypes/pr1.java
@@ -0,0 +1,17 @@
+
+public class pr1{
+    public static void main(String[] args) {
+        String stdname="Parshwa";
+
+        int age=19;
+        
+        double marks=80.5f;
+
+        System.out.println("Student name ="+stdname);
+        System.out.println("Student age = "+age);
+        System.out.println("Student marks = "+marks);
+
+        
+
+    }
+}
