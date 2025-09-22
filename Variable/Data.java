6223ed2d58df0b0cf66bade4fc5c0280ac68d54a
risu <risu794@gmail.com>
2025-09-22 14:00:15 +0530

    update code on 22-09-2025

--- Variable/Data.java
+++ Variable/Data.java
@@ -1,6 +1,22 @@
-public package Variable;
+//public package Variable;
 
-class Data {
+public class Data {
+    public static void main(String[] args) {
+        // data type
+    // int  age = 28;
+    // Double price = 10.50;
+    // char name = "risahv";
+    // boolean b = true;
 
-    
+    // // refrence data type
+    // // string 
+    // String str = "uiui";
+
+    // System.out.println(str);
+        
+    String name = "Rishav";
+    int age = 28;
+    double grade = 70.30;
+    System.out.println("name is " + name + "age is " + age + " Grade is " + grade);
+    }
 }
