0df4cac65a51d7f30b4ac46e9b8f56e8e0f98b35
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2025-09-22 13:12:29 +0530

    f

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First{
+    public static void main(String[] args) {
+        String name = "Balkrushna ";
+        String fullName = name.concat("Naik");
+        System.out.println(fullName);
+    }
+}
