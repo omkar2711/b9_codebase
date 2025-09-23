803199604821fcfac0fc66f019a1ac86ef3de3b7
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-23 21:07:17 +0530

    Create IncrementDecrementOperator.java file

---
+++ DSA/Arrays/operators/IncrementDecrementOperator.java
@@ -0,0 +1,14 @@
+package DSA.Arrays.operators;
+
+public class IncrementDecrementOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        //Increment  ++
+        //Decrement  --
+        System.out.println("a = "+a);
+        a++;
+        System.out.println("a after ++ :"+a); //+1
+        a--;
+        System.out.println("a after -- :"+a); //-1
+    }
+}
