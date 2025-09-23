e86710b9a975cad89df930a2f36f094cacfaf700
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-23 17:30:57 +0530

    Commit ArithmaticOperator.java

--- DSA/Arrays/operators/ArithmaticOperators.java
+++ DSA/Arrays/operators/ArithmaticOperators.java
@@ -1,5 +1,16 @@
 package DSA.Arrays.operators;
 
 public class ArithmaticOperators {
-    
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 50;
+        System.out.println("Addition of "+a+" and "+b+" is "+(a+b));//Addition
+        System.out.println("Substraction of "+a+" and "+b+" is "+(a-b));//Substraction
+        System.out.println("Multiplication of "+a+" and "+b+" is "+(a*b));//Multiplication
+        float div = a/b;
+        System.out.println("Division of "+a+" and "+b+" is "+div);//Division
+        System.out.println("Modulas of "+a+" and "+b+" is "+(a%b));
+
+    }
 }
