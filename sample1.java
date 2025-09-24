19bb4851be65f659487477dded674a7bfaebdad3
saurabhsurve257 <saurabhsurve257@gmail.com>
2025-09-24 12:28:30 +0530

    first commit on github

---
+++ sample1.java
@@ -0,0 +1,17 @@
+public class sample1 {
+    public static void main(String args[])
+    {
+        String name = "saurabh";
+        int age = 24;
+        char grad = 'A';
+
+        System.out.println("name:-"+name);
+        System.out.println("age:-"+age);
+        System.out.println("grad:- "+grad);
+
+    }
+
+
+
+    
+}
