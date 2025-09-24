53fbd8815fe2bfc5d746c730881f5c8199fdd132
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2025-09-24 14:00:18 +0530

    Thrid Program

---
+++ Third.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    //    System.out.println("Enter your name");
+        String name = sc.next();
+     //   System.out.println("Enter your hobbies");
+        String hobby = sc.next();
+        System.out.print("My name is : "+name );
+        System.out.print(" My hobbies is :"+hobby);
+
+      //  System.out.println("My name is :"+name + " My Hobbies are : "+hobby);
+    }
+}
