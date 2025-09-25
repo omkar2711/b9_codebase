dfe7e8b4896b0e6b03994f6bd5e5551d296a079d
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-25 12:50:58 +0530

    Create VoteDemo.java file

---
+++ DSA/Arrays/control_flow_statements/VoteDemo.java
@@ -0,0 +1,19 @@
+package DSA.Arrays.control_flow_statements;
+
+import java.util.Scanner;
+
+public class VoteDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age of Person who want to Vote :- ");
+        int age = sc.nextInt();
+        if(age >= 18)
+        {
+            System.out.println("You Can Vote");
+        }
+        else {
+            System.out.println("You Can Not Vote");
+        }
+        sc.close();
+    }
+}
