486be80c56062978f16c74e1e9dc65cc818b438a
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-25 13:04:17 +0530

    Create LeapYear.java file

---
+++ DSA/Arrays/control_flow_statements/LeapYear.java
@@ -0,0 +1,21 @@
+package DSA.Arrays.control_flow_statements;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Year to Check weather it is Leap Year or not :-");
+        int Year = sc.nextInt();
+
+        if(Year%4 == 0 || Year%100 == 0)
+        {
+            System.out.println(Year+ " is Leap Year");
+        }
+        else
+        {
+            System.out.println(Year+ " is Not Leap Year");
+        }
+
+    }
+}
