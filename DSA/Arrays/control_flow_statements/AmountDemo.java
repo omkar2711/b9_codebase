3ea8752da2d15384b68d84817e9ddd3326bab530
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-29 13:13:55 +0530

    Created Loops

--- DSA/Arrays/control_flow_statements/AmountDemo.java
+++ DSA/Arrays/control_flow_statements/AmountDemo.java
@@ -1,4 +1,4 @@
-﻿package DSA.Arrays.control_flow_statements;
+package DSA.Arrays.control_flow_statements;
 import java.util.Scanner;
 public class AmountDemo {
     public static void main(String[] args) {
