b8e13d1780d185893876fe2cf3da352fd0b08c7f
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-09-30 12:10:10 +0530

    Create Do While Questions

---
+++ DSA/Arrays/control_flow_statements/doWhile/NaturalNumbers.java
@@ -0,0 +1,12 @@
+package DSA.Arrays.control_flow_statements.doWhile;
+
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        int i = 1;
+        int n = 10;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=n);
+    }
+}
