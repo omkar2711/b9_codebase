3d925dbe6e7edd2df60e14b3e9710b26ae20c530
rushikesh bhagat <rushikesh@gmail.com>
2026-03-27 17:35:21 +0530

    pattern code

---
+++ ControlFlowStatement2/Patterns/simple.java
@@ -0,0 +1,56 @@
+package ControlFlowStatement2.Patterns;
+
+public class simple {
+    public static void main(String[] args) {
+        
+        // for(int i=1; i<=5; i++){
+
+        //     for(int j=5; j>=i; j--){
+        //         System.out.print("*");
+        //     }
+
+        //     for(int k=1; k<=i*2-2; k++){
+        //         System.out.print(" ");
+        //     }
+            
+        //     for(int l=5; l>=i; l--){
+        //         System.out.print("*");
+        //     }
+ 
+        //     System.out.println();
+        // }
+
+        // for(int i=1; i<=5; i++){
+
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+
+        //     for(int k=9; k>=i*2; k--){
+        //         System.out.print(" ");
+        //     }
+
+        //     for(int l=1; l<=i; l++){
+        //         System.out.print("*");
+        //     }   
+
+        //     System.out.println();
+        // }
+        
+
+        
+        for(int i=1; i<=5; i++){
+
+            for(int k=5; k>=i; k--){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<2*i; j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
